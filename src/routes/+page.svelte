fa3ba2d760f6164a636fe429f574e4b12b0599bf
Mark Connelly <com0004@gmail.com>
2023-02-26 15:35:25 +1100

    Created build cards

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,15 @@
 <script lang="ts">
+	import BuildCard from './BuildCard.svelte';
 </script>
 
-<div class="container h-full mx-auto flex justify-center items-center">
-	<div class="space-y-10 text-center">
+<div class="container h-full mx-auto justify-center items-center">
+	<div class="space-y-10 text-center my-16">
 		<h2 class="font-bold">Welcome to The Redstone Index.</h2>
+	</div>
+	<div class="flex gap-5 justify-center flex-wrap">
+		{#each [1, 2, 3, 4, 5] as _}
+			<BuildCard />
+		{/each}
 	</div>
 </div>
 
---
+++ src/routes/BuildCard.svelte
@@ -0,0 +1,30 @@
+<script lang="ts">
+	import { Avatar } from '@skeletonlabs/skeleton';
+
+	let title =
+		'Compact Instant 0-Tick 2 Wide Tileable Binary Adder Awesome Build Super cool mega project'.slice(
+			0,
+			Math.floor(Math.random() * 109)
+		);
+	let author = 'plasmatech8';
+	let imgSrc = `https://picsum.photos/500/500?i=${Math.random()}`;
+</script>
+
+<a href="/builds/0" class="card card-hover overflow-clip !w-96">
+	<!-- Image -->
+	<img src={imgSrc} alt="" class="object-cover w-full h-72" />
+	<!-- Title -->
+	<h5 class="overflow-hidden truncate font-bold tracking-tight px-3 py-1 text-lg">
+		{title}
+	</h5>
+	<!-- Author -->
+	<footer class="p-1 flex justify-start items-center space-x-4">
+		<Avatar src="https://i.pravatar.cc/160?img=48" width="w-10 flex-shrink-0" />
+		<div class="flex justify-between items-center opacity-70 grow">
+			<div class="max-w-[215px]">
+				<h6 class="font-bold truncate">By {author}</h6>
+			</div>
+			<small class="mr-3 whitespace-nowrap flex-shrink-0">On 26/02/2023</small>
+		</div>
+	</footer>
+</a>
