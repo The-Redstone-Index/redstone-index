147e2ec3d95c52caf5f0ff7f9bf281767722dc85
Mark Connelly <com0004@gmail.com>
2023-03-04 22:56:51 +1100

    Update landing page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,7 +2,7 @@
 	import BuildCard from './BuildCard.svelte';
 </script>
 
-<div class="container h-full mx-auto justify-center items-center">
+<div class="container h-full mx-auto justify-center items-center p-4">
 	<div class="space-y-10 text-center my-16">
 		<h2 class="font-bold">
 			Welcome to
@@ -10,17 +10,17 @@
 		</h2>
 	</div>
 	<section class="mb-8">
-		<h3 class="mb-2 font-semibold">Recent Builds</h3>
-		<div class="flex gap-5 overflow-x-visible overflow-y-visible">
-			{#each [1, 2, 3, 4, 5, 6] as _}
+		<h3 class="mb-5 font-semibold">Recent Builds</h3>
+		<div class="flex gap-5">
+			{#each [1, 2, 3, 4] as _}
 				<BuildCard />
 			{/each}
 		</div>
 	</section>
 	<section class="mb-8">
-		<h3 class="mb-2 font-semibold">Popular Builds</h3>
-		<div class="flex gap-5 overflow-x-visible overflow-y-visible">
-			{#each [1, 2, 3, 4, 5, 6] as _}
+		<h3 class="mb-5 font-semibold">Popular Builds</h3>
+		<div class="flex gap-5">
+			{#each [1, 2, 3, 4] as _}
 				<BuildCard />
 			{/each}
 		</div>
