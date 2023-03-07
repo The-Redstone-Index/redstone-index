b933efde2ebb3e694428273f686612ccae1a4351
Mark Connelly <com0004@gmail.com>
2023-03-07 21:40:03 +1100

    Update new build form

--- src/routes/builds/new/+page.svelte
+++ src/routes/builds/new/+page.svelte
@@ -54,161 +54,182 @@
 
 <div class="container mx-auto p-5">
 	<h1 class="my-8">Submit New Build</h1>
-	<div class="flex gap-10 flex-col mb-10">
-		<!-- Type -->
-		<div class="flex flex-col gap-2">
-			<label for="type" class="block mb-2">Type *</label>
-			<div class="grid w-full gap-6 md:grid-cols-3">
-				{#each types as t}
-					<div>
-						<input
-							type="radio"
-							id={'build-type-' + t.name}
-							name="build-type"
-							value={t.name}
-							class="hidden peer"
-							required
-						/>
-						<label
-							for={'build-type-' + t.name}
-							class="inline-flex h-full items-center justify-between w-full p-5  border border-surface-200 dark:border-surface-500 rounded-lg cursor-pointer peer-checked:border-primary-600 peer-checked:text-primary-600 card"
-						>
-							<div class="block">
-								<div class="w-full text-lg font-semibold">{t.name}</div>
-								<div class="w-full">{t.desc}s</div>
-							</div>
-						</label>
-					</div>
-				{/each}
-			</div>
-		</div>
-
-		<!-- Title -->
-		<div>
-			<label for="title" class="block mb-2">Title *</label>
-			<input
-				id="title"
-				class="input"
-				type="text"
-				placeholder="Write a title for this build... (e.g. Compact Instant 0-Tick 2 Wide Tileable Binary Adder)"
-			/>
-		</div>
-
-		<!-- Description -->
-		<div>
-			<label for="description" class="block mb-2">Description</label>
-			<textarea
-				id="description"
-				class="textarea"
-				placeholder="Write a summary of the build and outline any relevant details..."
-			/>
-		</div>
-
-		<!-- Specifications -->
-		<div class="flex flex-col gap-2">
-			<label for="specifications" class="block">Specifications</label>
-			<div class="table-container input-group">
-				<table class="table table-hover">
-					<thead class="text-left">
-						<th>Item</th>
-						<th>Value</th>
-						<th />
-					</thead>
-					<tbody>
-						{#each specifications as spec, i}
-							<tr class="focus-within:table-row-checked">
-								<td contenteditable="true" class="focus:outline-none table-cell-fit">
-									{spec.item}
-								</td>
-								<td contenteditable="true" class="focus:outline-none">
-									{spec.value}
-								</td>
-								<td class="table-cell-fit">
-									<button
-										class="btn-icon-sm"
-										on:click={() => {
-											specifications.splice(i, 1);
-											specifications = specifications;
-										}}
-									>
-										<i class="fa-solid fa-xmark" />
-									</button>
-								</td>
-							</tr>
-						{/each}
-						<tr
-							on:click={() => {
-								specifications = [...specifications, { item: '', value: '' }];
-							}}
-						>
-							<td class="font-bold cursor-pointer">+ Add Row</td>
-							<td />
-							<td />
-						</tr>
-					</tbody>
-				</table>
-			</div>
-		</div>
-
-		<!-- Schematic File -->
-		<div class="flex flex-col gap-2">
-			<label for="schematic-upload" class="block">Upload Schematic (NBT) *</label>
-			<input type="file" class="input" id="schematic-upload" bind:files={schematicFileList} />
-		</div>
-
-		<!-- Photos -->
-		<div class="flex flex-col gap-2">
-			<label for="photos-upload" class="block">Upload Photos (SVG, PNG, JPG, GIF)</label>
-			<input id="photos-upload" class="input" type="file" multiple bind:files={photosFileList} />
-			{#if photosFileList}
-				<ul class="w-fit list">
-					{#each Array.from(photosFileList) as item}
-						<li>
-							{item.name}
-						</li>
+	<form method="post">
+		<div class="flex gap-10 flex-col mb-10">
+			<!-- Type -->
+			<div class="flex flex-col gap-2">
+				<label for="type" class="block mb-2">Type * (select one)</label>
+				<div class="grid w-full gap-6 md:grid-cols-3">
+					{#each types as t}
+						<div>
+							<input
+								type="radio"
+								id={'build-type-' + t.name}
+								name="build-type"
+								value={t.name}
+								class="hidden peer"
+								required
+							/>
+							<label
+								for={'build-type-' + t.name}
+								class="inline-flex h-full items-center justify-between w-full p-5 border border-surface-200 dark:border-surface-500 rounded-3xl cursor-pointer peer-checked:border-primary-600 peer-checked:text-primary-600 dark:peer-checked:border-primary-500 dark:peer-checked:text-primary-500 card card-hover"
+							>
+								<div class="block">
+									<div class="w-full text-lg font-semibold">{t.name}</div>
+									<div class="w-full">{t.desc}s</div>
+								</div>
+							</label>
+						</div>
 					{/each}
-				</ul>
-			{/if}
-		</div>
+				</div>
+			</div>
 
-		<!-- Version Compatability -->
-		<div class="flex flex-col gap-2">
-			<label for="versions" class="block">Version Compatability</label>
+			<!-- Title -->
 			<div>
-				<button color="primary">Compatible In...</button>
-				Dropdown...
+				<label for="title" class="block mb-2">Title *</label>
+				<input
+					id="title"
+					class="input"
+					type="text"
+					placeholder="Write a title for this build... (e.g. Compact Instant 0-Tick 2 Wide Tileable Binary Adder)"
+					name="title"
+				/>
 			</div>
-			<div class="flex flex-nowrap gap-3 overflow-x-auto p-1 text-gray-600 dark:text-gray-300">
-				{#each versions.sort((a, b) => {
-					if (a.toLowerCase().includes('breaks') && !b.toLowerCase().includes('breaks')) {
-						return -1;
-					}
-					if (a > b) return 1;
-					return 0;
-				}) as version}
-					<div
-						class="rounded-md bg-gray-50 p-2 outline outline-2 outline-gray-200 dark:bg-gray-900 dark:outline-gray-700"
-						class:text-red-500={version.toLowerCase().includes('breaks')}
-					>
-						{version}
-					</div>
-				{/each}
+
+			<!-- Description -->
+			<div>
+				<label for="description" class="block mb-2">Description</label>
+				<textarea
+					id="description"
+					class="textarea"
+					placeholder="Write a summary of the build and outline any relevant details..."
+					name="description"
+				/>
 			</div>
-		</div>
 
-		<!-- Tags -->
-		<div>
-			<label for="tags" class="block mb-2">Tags</label>
-			<input
-				id="tags"
-				type="text"
-				placeholder="e.g. Compact Instant 0-Tick 2 Wide Tileable Binary Adder"
-				class="input"
-				bind:value={tags}
-			/>
-		</div>
+			<!-- Specifications -->
+			<div class="flex flex-col gap-2">
+				<label for="specifications" class="block">Specifications (edit table)</label>
+				<div class="table-container input-group">
+					<table class="table table-hover">
+						<thead class="text-left">
+							<th>Item</th>
+							<th>Value</th>
+							<th />
+						</thead>
+						<tbody>
+							{#each specifications as spec, i}
+								<tr class="focus-within:table-row-checked">
+									<td
+										contenteditable="true"
+										class="focus:outline-none table-cell-fit min-w-[250px]"
+									>
+										{spec.item}
+									</td>
+									<td contenteditable="true" class="focus:outline-none">
+										{spec.value}
+									</td>
+									<td class="table-cell-fit">
+										<button
+											class="btn-icon-sm"
+											on:click={() => {
+												specifications.splice(i, 1);
+												specifications = specifications;
+											}}
+										>
+											<i class="fa-solid fa-xmark" />
+										</button>
+									</td>
+								</tr>
+							{/each}
+							<tr
+								on:click={() => {
+									specifications = [...specifications, { item: '', value: '' }];
+								}}
+							>
+								<td class="font-bold cursor-pointer">+ Add Row</td>
+								<td />
+								<td />
+							</tr>
+						</tbody>
+					</table>
+				</div>
+			</div>
 
-		<!-- Submit Button -->
-		<button class="btn variant-filled-primary md:w-fit">Publish</button>
-	</div>
+			<!-- Schematic File -->
+			<div class="flex flex-col gap-2">
+				<label for="schematic-upload" class="block">Upload Schematic (NBT) *</label>
+				<input
+					type="file"
+					class="input"
+					id="schematic-upload"
+					name="schematic-upload"
+					bind:files={schematicFileList}
+				/>
+			</div>
+
+			<!-- Photos -->
+			<div class="flex flex-col gap-2">
+				<label for="photos-upload" class="block">Upload Photos (SVG, PNG, JPG, GIF)</label>
+				<input
+					id="photos-upload"
+					class="input"
+					name="photos-upload"
+					type="file"
+					multiple
+					bind:files={photosFileList}
+				/>
+				{#if photosFileList}
+					<ul class="w-fit list">
+						{#each Array.from(photosFileList) as item}
+							<li>
+								{item.name}
+							</li>
+						{/each}
+					</ul>
+				{/if}
+			</div>
+
+			<!-- Version Compatability -->
+			<div class="flex flex-col gap-2">
+				<label for="versions" class="block">Version Compatability</label>
+				<div>
+					<button color="primary">Compatible In...</button>
+					Dropdown...
+				</div>
+				<div class="flex flex-nowrap gap-3 overflow-x-auto p-1 text-gray-600 dark:text-gray-300">
+					{#each versions.sort((a, b) => {
+						if (a.toLowerCase().includes('breaks') && !b.toLowerCase().includes('breaks')) {
+							return -1;
+						}
+						if (a > b) return 1;
+						return 0;
+					}) as version}
+						<div
+							class="rounded-md bg-gray-50 p-2 outline outline-2 outline-gray-200 dark:bg-gray-900 dark:outline-gray-700"
+							class:text-red-500={version.toLowerCase().includes('breaks')}
+						>
+							{version}
+						</div>
+					{/each}
+				</div>
+			</div>
+
+			<!-- Tags -->
+			<div>
+				<label for="tags" class="block mb-2">Tags</label>
+				<input
+					id="tags"
+					type="text"
+					name="tags"
+					placeholder="e.g. Compact Instant 0-Tick 2 Wide Tileable Binary Adder"
+					class="input"
+					bind:value={tags}
+				/>
+			</div>
+
+			<!-- Submit Button -->
+			<button class="btn variant-filled-primary md:w-fit" type="submit">Publish</button>
+		</div>
+	</form>
 </div>
