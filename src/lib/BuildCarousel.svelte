ddd8ee56022192fea5b46738916cd7fa095c76a5
Mark Connelly <com0004@gmail.com>
2023-03-19 22:04:34 +1100

    Add build carousel

---
+++ src/lib/BuildCarousel.svelte
@@ -0,0 +1,48 @@
+<script lang="ts">
+	import BuildCard from '../routes/BuildCard.svelte';
+
+	export let items = [...Array(20).keys()];
+
+	let scrollContainer: Element;
+	let scrollIndex = 0;
+
+	$: atStart = scrollIndex === 0;
+	$: atEnd = scrollIndex === items.length - 1;
+
+	const move = (direction: number) => {
+		scrollIndex += direction;
+		scrollContainer.children[scrollIndex].scrollIntoView({ inline: 'start', block: 'nearest' });
+	};
+</script>
+
+<div class="relative overflow-hidden">
+	<div
+		class="gap-4 p-5 no-scrollbar flex overflow-x-scroll scroll-smooth"
+		bind:this={scrollContainer}
+	>
+		{#each items as item, i}
+			<BuildCard />
+		{/each}
+	</div>
+	<button
+		class="btn-icon variant-filled-primary absolute left-3 top-48"
+		disabled={atStart}
+		on:click={() => move(-1)}><i class="fa-solid fa-caret-left" /></button
+	>
+	<button
+		class="btn-icon variant-filled-primary absolute right-3 top-48"
+		disabled={atEnd}
+		on:click={() => move(1)}><i class="fa-solid fa-caret-right" /></button
+	>
+</div>
+
+<style>
+	.no-scrollbar::-webkit-scrollbar {
+		display: none;
+	}
+	/* Hide scrollbar for IE, Edge and Firefox */
+	.no-scrollbar {
+		-ms-overflow-style: none; /* IE and Edge */
+		scrollbar-width: none; /* Firefox */
+	}
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-	import BuildCard from './BuildCard.svelte';
+	import BuildCarousel from '$lib/BuildCarousel.svelte';
 </script>
 
 <div class="container h-full mx-auto justify-center items-center p-4">
@@ -9,21 +9,16 @@
 			<span class="text-redstone">The Redstone Index </span>
 		</h2>
 	</div>
+
+	<div class="w-full bg-red-800" />
+
 	<section class="mb-8">
 		<h3 class="mb-5 font-semibold">Recent Builds</h3>
-		<div class="flex gap-5">
-			{#each [1, 2, 3, 4] as _}
-				<BuildCard />
-			{/each}
-		</div>
+		<BuildCarousel />
 	</section>
 	<section class="mb-8">
 		<h3 class="mb-5 font-semibold">Popular Builds</h3>
-		<div class="flex gap-5">
-			{#each [1, 2, 3, 4] as _}
-				<BuildCard />
-			{/each}
-		</div>
+		<BuildCarousel />
 	</section>
 </div>
 
