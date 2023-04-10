fecdec2317f09f544befdc9ad6470cdc31c0c14a
Mark Connelly <com0004@gmail.com>
2023-04-10 22:54:48 +1000

    Add update profile button

--- src/routes/users/[id]/+page.svelte
+++ src/routes/users/[id]/+page.svelte
@@ -20,6 +20,7 @@
 	<div class="flex mb-5 items-center gap-5">
 		<Avatar width="w-24" src="https://i.pravatar.cc/" cursor="cursor-pointer" />
 		<h1>plasmatech8</h1>
+		<a href="/users/0/settings">Update profile</a>
 	</div>
 
 	<div class="mb-5 flex gap-3 justify-end items-center">
