35e93eb44882880dfe040aacf841dcd7ace9f557
Mark Connelly <com0004@gmail.com>
2023-07-21 23:09:01 +1000

    Add specs table editor to detail section

--- src/routes/builds/[id]/DetailsSection.svelte
+++ src/routes/builds/[id]/DetailsSection.svelte
@@ -1,22 +1,29 @@
 <script lang="ts">
-	export let stats: Array<{ item: string; value: string | number }>;
+	import SpecsTableEditor from '$lib/SpecsTableEditor.svelte';
+
+	export let stats: Array<{ item: string; value: string }>;
+	export let editing: Boolean;
 </script>
 
 <section>
 	<div>
-		<table class="table table-hover">
-			<thead class="text-xs uppercase text-left">
-				<th>Item</th>
-				<th>Value</th>
-			</thead>
-			<tbody>
-				{#each stats as stat}
-					<tr>
-						<td class="table-cell-fit">{stat.item}</td>
-						<td>{stat.value}</td>
-					</tr>
-				{/each}
-			</tbody>
-		</table>
+		{#if editing}
+			<SpecsTableEditor specifications={stats} />
+		{:else}
+			<table class="table table-hover">
+				<thead class="text-xs uppercase text-left">
+					<th>Item</th>
+					<th>Value</th>
+				</thead>
+				<tbody>
+					{#each stats as stat}
+						<tr>
+							<td class="table-cell-fit">{stat.item}</td>
+							<td>{stat.value}</td>
+						</tr>
+					{/each}
+				</tbody>
+			</table>
+		{/if}
 	</div>
 </section>
