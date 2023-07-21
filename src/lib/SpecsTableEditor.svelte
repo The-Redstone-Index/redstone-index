4a001ad97fe7fbef668ad0bb5fa44478a27413b1
Mark Connelly <com0004@gmail.com>
2023-07-21 23:08:04 +1000

    Inprove summary section

---
+++ src/lib/SpecsTableEditor.svelte
@@ -0,0 +1,43 @@
+<script lang="ts">
+	export let specifications: { item: String; value: String }[];
+</script>
+
+<table class="table table-hover">
+	<thead class="text-left">
+		<th>Item</th>
+		<th>Value</th>
+		<th />
+	</thead>
+	<tbody>
+		{#each specifications as spec, i}
+			<tr class="focus-within:table-row-checked">
+				<td contenteditable="true" class="focus:outline-none table-cell-fit min-w-[250px]">
+					{spec.item}
+				</td>
+				<td contenteditable="true" class="focus:outline-none">
+					{spec.value}
+				</td>
+				<td class="table-cell-fit">
+					<button
+						class="btn-icon-sm"
+						on:click={() => {
+							specifications.splice(i, 1);
+							specifications = specifications;
+						}}
+					>
+						<i class="fa-solid fa-xmark" />
+					</button>
+				</td>
+			</tr>
+		{/each}
+		<tr
+			on:click={() => {
+				specifications = [...specifications, { item: '', value: '' }];
+			}}
+		>
+			<td class="font-bold cursor-pointer">+ Add Row</td>
+			<td />
+			<td />
+		</tr>
+	</tbody>
+</table>
