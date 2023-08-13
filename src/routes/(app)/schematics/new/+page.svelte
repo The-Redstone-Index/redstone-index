8cb1ddd8ecc5b94d86ed931d247a6e2b137e150b
Mark Connelly <com0004@gmail.com>
2023-08-13 17:03:57 +1000

    Add new schematic page back

---
+++ src/routes/(app)/schematics/new/+page.svelte
@@ -0,0 +1,97 @@
+<script lang="ts">
+	import { goto } from '$app/navigation';
+	import StructureViewer from '$lib/minecraft-rendering/StructureViewer.svelte';
+	import { getResources } from '$lib/minecraft-rendering/helpers';
+	import { FileDropzone, RadioGroup, RadioItem } from '@skeletonlabs/skeleton';
+	import type { FileDropzoneEvents } from '@skeletonlabs/skeleton/dist/components/FileDropzone/FileDropzone.svelte';
+	import type { Resources } from 'deepslate';
+	import { onMount } from 'svelte';
+	let uploadMethod = 0;
+	// let files: FileList;
+	// $: file = files?.item(0);
+	let schemaData: ArrayBuffer | null;
+	let resources: Resources;
+	let viewerClientWidth = 0; // For key block when window resized
+
+	function handleFileSelect(event: Event) {
+		const fileList = (event.target as HTMLInputElement).files;
+		if (!fileList || fileList.length === 0) {
+			return (schemaData = null);
+		}
+		const file = fileList[0] as File;
+
+		// Read the file content
+		const reader = new FileReader();
+		reader.onload = (event: ProgressEvent<FileReader>) => {
+			const fileData = event.target?.result as ArrayBuffer;
+			schemaData = fileData;
+		};
+		reader.readAsArrayBuffer(file);
+	}
+
+	onMount(async () => {
+		resources = await getResources();
+	});
+</script>
+
+<svelte:head>
+	<title>New Schematic - Redstone Index</title>
+</svelte:head>
+
+<div class="container mx-auto flex flex-col gap-5 p-5">
+	<h1 class="my-8">Upload a New Schematic</h1>
+
+	{#if !schemaData}
+		<div class="mx-auto mb-5">
+			<RadioGroup active="variant-filled-primary" hover="hover:variant-soft-primary">
+				<RadioItem bind:group={uploadMethod} name="nbt" value={0}>NBT File</RadioItem>
+				<RadioItem bind:group={uploadMethod} name="world" value={1} disabled>
+					World Folder + Coords
+				</RadioItem>
+				<RadioItem bind:group={uploadMethod} name="litematic" value={2} disabled>
+					Litematic File
+				</RadioItem>
+			</RadioGroup>
+		</div>
+
+		<FileDropzone
+			name="files"
+			class="py-20 h-64 md:h-96 max-w-4xl mx-auto"
+			on:change={handleFileSelect}
+			accept=".nbt"
+		>
+			<svelte:fragment slot="lead">
+				<i class="fa-solid fa-cube text-5xl" />
+			</svelte:fragment>
+			<svelte:fragment slot="message">
+				<b>Upload a file</b>
+				or drag and drop
+			</svelte:fragment>
+			<svelte:fragment slot="meta">NBT File (max size ? GB)</svelte:fragment>
+		</FileDropzone>
+	{:else}
+		<div
+			class="w-full md:h-[700px] bg-surface-800 aspect-square md:aspect-auto rounded-xl"
+			bind:clientWidth={viewerClientWidth}
+		>
+			{#key viewerClientWidth}
+				<StructureViewer {schemaData} {resources} doElevationSlider doInputControls doBlockList />
+			{/key}
+		</div>
+	{/if}
+
+	{#if schemaData}
+		<div class="flex justify-end gap-3">
+			<button type="button" class="btn variant-filled" on:click={() => (schemaData = null)}>
+				Clear
+			</button>
+			<button
+				type="button"
+				class="btn variant-filled-primary"
+				on:click={() => goto('/users/0#schematics')}
+			>
+				Upload
+			</button>
+		</div>
+	{/if}
+</div>
