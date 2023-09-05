3f59f9e4ae4e6d3eb6f072c6422d742f58be8b4a
Mark Connelly <com0004@gmail.com>
2023-09-05 21:00:50 +1000

    Fix face api type

--- src/routes/api/face/[username]/+server.ts
+++ src/routes/api/face/[username]/+server.ts
@@ -1,5 +1,5 @@
 import { error } from '@sveltejs/kit';
-import type { RequestHandler } from '../$types';
+import type { RequestHandler } from './$types';
 
 export const GET: RequestHandler = async ({ params, fetch }) => {
 	try {
