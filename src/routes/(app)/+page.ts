02244edf7a8ca0931a010d966e58a42e67cf9ff8
Mark Connelly <com0004@gmail.com>
2023-10-16 22:16:44 +1100

    Fix type error

--- src/routes/(app)/+page.ts
+++ src/routes/(app)/+page.ts
@@ -1,11 +1,11 @@
-import type { PageLoad } from './$types';
 import { getRecentBuilds } from '$lib/api';
+import type { PageLoad } from './$types';
 
 export const load: PageLoad = async ({ parent }) => {
 	const { supabase } = await parent();
 
 	const recentBuilds = await getRecentBuilds(supabase);
-	const popularBuilds = [];
+	const popularBuilds: unknown = [];
 	return {
 		recentBuilds: (recentBuilds as unknown as BuildDetails[]) ?? [],
 		popularBuilds: (popularBuilds as unknown as BuildDetails[]) ?? []
