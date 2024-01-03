f2e6189b2cd0f7a384af3b3135e1c0e23ce9fcb2
Mark Connelly <com0004@gmail.com>
2024-01-03 17:08:59 +1100

    Remove unused imports

--- src/routes/(app)/builds/[id]/AssetViewerSection.svelte
+++ src/routes/(app)/builds/[id]/AssetViewerSection.svelte
@@ -4,10 +4,8 @@
 	import LoadingSpinnerArea from '$lib/common/LoadingSpinnerArea.svelte';
 	import StaticStructurePreview from '$lib/minecraft/StaticStructurePreview.svelte';
 	import StructureViewer from '$lib/minecraft/StructureViewer.svelte';
-	import { getResources } from '$lib/minecraft/mcmetaAPI';
 	import { minecraftStore } from '$lib/stores';
 	import type { Resources } from 'deepslate';
-	import { onMount } from 'svelte';
 
 	export let supabase: SupabaseClient;
 	export let schematicPath: string;
