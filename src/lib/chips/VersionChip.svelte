1bc0ef648204e5ebae3d41345979e0779287d949
Mark Connelly <com0004@gmail.com>
2024-01-06 18:54:08 +1100

    Remove unused import, fix text color on 'tested'

--- src/lib/chips/VersionChip.svelte
+++ src/lib/chips/VersionChip.svelte
@@ -1,13 +1,12 @@
 <script lang="ts">
 	import { minecraftStore } from '$lib/stores';
-	import { createEventDispatcher } from 'svelte';
 
 	export let version: number;
 	export let type: 'working' | 'breaking' | 'tested' | 'none' = 'none';
 </script>
 
 <div
-	class="chip variant-filled inline-block w-fit h-fit"
+	class="chip variant-filled inline-block w-fit h-fit text-black"
 	class:variant-filled-success={type === 'working'}
 	class:variant-filled-error={type === 'breaking'}
 	class:!bg-secondary-400={type === 'tested'}
