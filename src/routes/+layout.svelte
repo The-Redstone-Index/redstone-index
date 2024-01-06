ee30a9abb763996c3339d72eead9a28c662ddb86
Mark Connelly <com0004@gmail.com>
2024-01-06 20:16:03 +1100

    Make always scroll to top after navigating

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,8 +1,9 @@
 <script lang="ts">
-	import { invalidate } from '$app/navigation';
+	import { afterNavigate, invalidate } from '$app/navigation';
 	import { supabaseStore } from '$lib/stores';
 	import '@fortawesome/fontawesome-free/js/all.min.js';
 	import { initializeStores } from '@skeletonlabs/skeleton';
+	import type { AfterNavigate } from '@sveltejs/kit';
 	import { onMount } from 'svelte';
 	import '../app.postcss';
 
@@ -10,12 +11,23 @@
 	let { supabase, session } = data;
 	$: ({ supabase, session } = data);
 
+	// Init Skeleton stores (modals & toasts)
 	initializeStores();
 
+	// Init Supabase store
 	supabaseStore.set(supabase);
 
+	// Always scroll to top when navigating to a new page
+	afterNavigate((params: AfterNavigate) => {
+		const isNewPage = params.from?.url.pathname !== params.to?.url.pathname;
+		const elemPage = document.querySelector('#page');
+		if (isNewPage && elemPage !== null) {
+			elemPage.scrollTop = 0;
+		}
+	});
+
+	// Invalidate session related data when auth state changes
 	onMount(() => {
-		// Invalidate session related data when auth state changes
 		const { data } = supabase.auth.onAuthStateChange((_, _session) => {
 			if (_session?.expires_at !== session?.expires_at) {
 				invalidate('supabase:auth');
