bb765290b0ea1a3ab9500fcab3c6c400c664c054
Mark Connelly <com0004@gmail.com>
2024-01-06 19:48:43 +1100

    remove top level await on minecraft store

--- src/lib/stores.ts
+++ src/lib/stores.ts
@@ -2,19 +2,30 @@ import { browser } from '$app/environment';
 import { get, readable, writable } from 'svelte/store';
 import { getBlockList, getResources, getVersionList } from './minecraft/mcmetaAPI';
 
-async function createMinecraftStore() {
-	if (!browser) return null;
-	const resources = await getResources();
-	const blockList = await getBlockList();
-	const versionList = await getVersionList();
-	const store = readable({
-		resources,
-		blockList,
-		versionList
+interface MinecraftStore {
+	resources: Awaited<ReturnType<typeof getResources>>;
+	blockList: Awaited<ReturnType<typeof getBlockList>>;
+	versionList: Awaited<ReturnType<typeof getVersionList>>;
+}
+
+function createMinecraftStore() {
+	const store = readable<null | MinecraftStore>(null, (set) => {
+		// Do not fetch on server
+		if (!browser) return;
+		// Fetch Minecraft data
+		Promise.all([getResources(), getBlockList(), getVersionList()]).then(
+			([resources, blockList, versionList]) => {
+				set({
+					resources,
+					blockList,
+					versionList
+				});
+			}
+		);
 	});
 
 	function getVersionName(dataVersion: number) {
-		const version = get(store).versionList.find((v) => v.data_version === dataVersion);
+		const version = get(store)?.versionList.find((v) => v.data_version === dataVersion);
 		return version?.name;
 	}
 
@@ -24,6 +35,6 @@ async function createMinecraftStore() {
 	};
 }
 
-export const minecraftStore = await createMinecraftStore();
+export const minecraftStore = createMinecraftStore();
 
 export const supabaseStore = writable<SupabaseClient>();
