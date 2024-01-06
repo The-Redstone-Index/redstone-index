eaeada119a602d3e025facdc9d3f5f494b6b555d
Mark Connelly <com0004@gmail.com>
2024-01-06 16:26:39 +1100

    Add API docs page

---
+++ src/routes/(app)/documentation/+page.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+	import { marked } from 'marked';
+	import { onMount } from 'svelte';
+	import tos from './docs.md?raw';
+
+	onMount(() => {});
+</script>
+
+<svelte:head>
+	<title>API Documentation - The Redstone Index</title>
+	<meta name="description" content="API documentation for The Redstone Index." />
+</svelte:head>
+
+<div class="container mx-auto p-5 mb-14">
+	<section class="prose">
+		{@html marked(tos, { gfm: true })}
+	</section>
+</div>
