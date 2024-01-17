e52747f6e725c4ad7c2030243cde2d4f676cb5a5
Mark Connelly <com0004@gmail.com>
2024-01-17 15:42:30 +1100

    Update info pages, add donate and feedback pages (unfinished)

---
+++ src/routes/(app)/(info)/+layout.svelte
@@ -0,0 +1,8 @@
+<div class="container mx-auto p-5 pb-0">
+	<a href="." class="anchor">
+		<i class="fa-solid fa-angles-left mr-1" />
+		Back to Home
+	</a>
+</div>
+
+<slot />
---
+++ src/routes/(app)/(info)/donate/+page.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+	import { marked } from 'marked';
+	import donations from './donations.md?raw';
+</script>
+
+<svelte:head>
+	<title>Feedback - The Redstone Index</title>
+	<meta
+		name="description"
+		content="About the Redstone Index platform. Explore, share, and discover Redstone builds."
+	/>
+</svelte:head>
+
+<div class="container mx-auto p-5 mb-14">
+	<section class="prose">
+		{@html marked(donations, { gfm: true })}
+	</section>
+</div>
