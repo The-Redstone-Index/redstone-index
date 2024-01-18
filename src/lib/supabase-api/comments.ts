32f14dd9ab2733baf667b1f89b3b978eb0a0a848
Mark Connelly <com0004@gmail.com>
2024-01-19 01:53:54 +1100

    Add pagination,

--- src/lib/supabase-api/comments.ts
+++ src/lib/supabase-api/comments.ts
@@ -2,15 +2,23 @@ export async function getComments(
 	supabase: SupabaseClient,
 	{ buildId, offset = 0, limit = 25 }: { buildId: number; offset?: number; limit?: number }
 ) {
-	// 				author:users!builds_user_id_fkey(*),
-
-	const { data, error } = await supabase
+	// Construct query
+	const query = supabase
 		.from('comments')
-		.select('*,  author:users!user_id(*), parent:replying_to(*, author:users!user_id(*))')
+		.select('*,  author:users!user_id(*), parent:replying_to(*, author:users!user_id(*))', {
+			count: 'estimated'
+		})
 		.eq('build_id', buildId)
 		.range(offset, offset + limit - 1);
+
+	// Order by date
+	query.order('created_at', { ascending: false });
+
+	// Return results
+	const { data, count, error } = await query;
 	if (error) console.error(error);
-	return [data as unknown as CommentDetails[], error] as const;
+	console.log(count);
+	return [data as unknown as CommentDetails[], error, count] as const;
 }
 
 export async function getSingleComment(supabase: SupabaseClient, id: number) {
--- src/routes/(app)/(content)/builds/[id]/CommentsSection.svelte
+++ src/routes/(app)/(content)/builds/[id]/CommentsSection.svelte
@@ -3,7 +3,8 @@
 	import AutoResizeTextarea from '$lib/inputs/AutoResizeTextarea.svelte';
 	import { supabaseStore } from '$lib/stores';
 	import { getComments, getSingleComment } from '$lib/supabase-api/comments';
-	import { getToastStore } from '@skeletonlabs/skeleton';
+	import type { SortingOption } from '$lib/types';
+	import { Paginator, getToastStore } from '@skeletonlabs/skeleton';
 	import { createEventDispatcher, onMount } from 'svelte';
 	import { slide } from 'svelte/transition';
 	import Comment from './Comment.svelte';
@@ -12,7 +13,7 @@
 	export let buildId: number;
 	export let userId: string | undefined;
 	export let offset: number = 0;
-	export let limit: number = 20;
+	export let limit: number = 10;
 	export let highlightedCommentId: number | undefined = undefined;
 
 	const supabase = $supabaseStore;
@@ -31,10 +32,14 @@
 			return data;
 		});
 
-	$: commentsQuery = getComments(supabase, { offset, limit, buildId }).then(([data, error]) => {
-		if (error || !data) throw error;
-		return data;
-	});
+	$: commentsQuery = getComments(supabase, { offset, limit, buildId }).then(
+		([data, error, count]) => {
+			if (error || !data) throw error;
+			commentsCount = count ?? 0;
+			return data;
+		}
+	);
+	let commentsCount: number = 0;
 
 	onMount(() => {
 		handleScrollToHighlightedComment();
@@ -101,6 +106,18 @@
 		highlightedCommentId = data.id;
 		dispatch('commented');
 	}
+
+	// Pagination
+
+	async function onAmountChange(e: CustomEvent) {
+		const newAmount = e.detail as number;
+		limit = newAmount;
+	}
+
+	async function onPageChange(e: CustomEvent) {
+		const newPage = e.detail as number;
+		offset = newPage * limit;
+	}
 </script>
 
 <section id="#comments" class="flex flex-col gap-4">
@@ -146,6 +163,7 @@
 			</button>
 		</div>
 	</form>
+
 	<!-- Highlighted comment -->
 	{#if highlightedCommentQuery}
 		<div bind:this={highlightedCommentContainerEl}>
@@ -160,6 +178,18 @@
 			{/await}
 		</div>
 	{/if}
+
+	<!-- Sorting -->
+	<!-- <div class="px-[3.5rem] flex justify-end">
+		<label class="label flex gap-3 items-center">
+			<span class="text-sm whitespace-nowrap">Sort By</span>
+			<select class="select text-sm">
+				<option value="1">Oldest First</option>
+				<option value="2">Newest First</option>
+			</select>
+		</label>
+	</div> -->
+
 	<!-- Comments -->
 	<div class="flex flex-col gap-3">
 		{#await commentsQuery then comments}
@@ -171,5 +201,14 @@
 				/>
 			{/each}
 		{/await}
+	</div>
+
+	<!-- Paginator -->
+	<div class="px-[3.5rem]">
+		<Paginator
+			settings={{ limit, page: 0, amounts: [10, 30], size: commentsCount }}
+			on:amount={onAmountChange}
+			on:page={onPageChange}
+		/>
 	</div>
 </section>
