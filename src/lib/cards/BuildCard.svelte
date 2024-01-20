b7d3f8cbcf9cef23957c9f6a106233c31da36c5c
Mark Connelly <com0004@gmail.com>
2024-01-20 17:47:09 +1100

    Fix build card reload bug, fix search screen key bug, add structure size render guard

--- src/lib/cards/BuildCard.svelte
+++ src/lib/cards/BuildCard.svelte
@@ -2,18 +2,18 @@
 	import { browser } from '$app/environment';
 	import LoadingSpinnerArea from '$lib/common/LoadingSpinnerArea.svelte';
 	import StaticStructurePreview from '$lib/minecraft/StaticStructurePreview.svelte';
+	import StructureSizeLimitGuard from '$lib/minecraft/StructureSizeLimitGuard.svelte';
 	import StructureViewer from '$lib/minecraft/StructureViewer.svelte';
 	import { getStructureSize } from '$lib/minecraft/utils';
 	import { minecraftStore, supabaseStore } from '$lib/stores';
 	import { getAvatarUrl } from '$lib/supabase-api/storage';
 	import { Avatar } from '@skeletonlabs/skeleton';
-	import type { Resources } from 'deepslate';
 	import { onMount } from 'svelte';
 
 	export let build: BuildCardDetails;
 	export let to: string | undefined = undefined;
 
-	const resources: Resources | undefined = $minecraftStore?.resources;
+	$: resources = $minecraftStore?.resources;
 	const supabase: SupabaseClient = $supabaseStore;
 
 	let titleHeight = 0;
@@ -74,16 +74,18 @@
 					<i class="fa-solid fa-circle-exclamation animate-pulse" />
 				</div>
 			{:else if resources && browser && schemaData}
-				<div class="flex flex-col" class:flex-col-reverse={loaded && hovering}>
-					<div class="w-80 h-72">
-						<StaticStructurePreview {schemaData} {resources} />
+				<StructureSizeLimitGuard {schemaData} showContinue={hovering}>
+					<div class="flex flex-col" class:flex-col-reverse={loaded && hovering}>
+						<div class="w-80 h-72">
+							<StaticStructurePreview {schemaData} {resources} />
+						</div>
+						<div class="w-80 h-72">
+							{#if hovering}
+								<StructureViewer {schemaData} {resources} doStaticRotation bind:loaded />
+							{/if}
+						</div>
 					</div>
-					<div class="w-80 h-72">
-						{#if hovering}
-							<StructureViewer {schemaData} {resources} doStaticRotation bind:loaded />
-						{/if}
-					</div>
-				</div>
+				</StructureSizeLimitGuard>
 			{:else}
 				<div class="h-72">
 					<LoadingSpinnerArea />
