8270711fcca76c01333c91434317e54a9b5441f5
Mark Connelly <com0004@gmail.com>
2024-01-21 14:39:16 +1100

    Add mod/admin chips

---
+++ src/lib/chips/UserRoleChip.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	import { getRoleShortName } from '$lib/utils';
+
+	export let user: Tables<'users'>;
+	export let compact: boolean = false;
+</script>
+
+<div class="variant-outline-primary btn btn-sm text-xs" class:py-1={compact} class:px-2={compact}>
+	{getRoleShortName(user.role)}
+</div>
