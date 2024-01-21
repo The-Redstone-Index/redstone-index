7f482da49592f3d93d031f9a505a2670adb9f166
Mark Connelly <com0004@gmail.com>
2024-01-21 22:12:44 +1100

    Fix build removal modals

--- src/routes/(app)/(content)/builds/[id]/BuildEllipsesMenu.svelte
+++ src/routes/(app)/(content)/builds/[id]/BuildEllipsesMenu.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+	import { invalidateAll } from '$app/navigation';
 	import { supabaseStore } from '$lib/stores';
 	import { submitUserReport } from '$lib/supabase-api/reports';
 	import { getModalStore, getToastStore } from '@skeletonlabs/skeleton';
@@ -48,19 +49,15 @@
 	function openRemoveBuildModal() {
 		modalStore.trigger({
 			type: 'confirm',
-			title: build.removed ? 'Un-Remove' : 'Remove Build',
-			body: build.removed
-				? `<p>Make this build visible again on the index.</p>
-				<br>
-				<p>(Build was miscategorised or is no longer deemed inappropriate content)</p>`
-				: `<p>Remove visibility of this build on the index.</p>
+			title: 'Remove Build',
+			body: `<p>Remove visibility of this build on the index.</p>
 				<br>
 				<p>(This build is inappropriate or the author has asked for removal)</p>`,
 			response: async (r) => {
 				if (r) {
 					const { error } = await supabase
 						.from('builds')
-						.update({ removed: !build.removed })
+						.update({ removed: true })
 						.eq('id', build.id);
 					if (error) {
 						toastStore.trigger({
@@ -75,6 +72,40 @@
 						background: 'variant-filled-success',
 						classes: 'pl-8'
 					});
+					build.removed = !build.removed;
+				}
+				invalidateAll();
+			}
+		});
+	}
+
+	function openUnRemoveBuildModal() {
+		modalStore.trigger({
+			type: 'confirm',
+			title: 'Un-Remove',
+			body: `<p>Make this build visible again on the index.</p>
+				<br>
+				<p>(Build was miscategorised or is no longer deemed inappropriate content)</p>`,
+			response: async (r) => {
+				if (r) {
+					const { error } = await supabase
+						.from('builds')
+						.update({ removed: false })
+						.eq('id', build.id);
+					if (error) {
+						toastStore.trigger({
+							message: `<i class="fas fa-triangle-exclamation mr-1"></i> ${error.message}`,
+							background: 'variant-filled-error',
+							classes: 'pl-8'
+						});
+						return;
+					}
+					toastStore.trigger({
+						message: `<i class="fas fa-check mr-1"></i> Build Un-Removed!`,
+						background: 'variant-filled-success',
+						classes: 'pl-8'
+					});
+					invalidateAll();
 				}
 			}
 		});
@@ -97,20 +128,27 @@
 		{/if}
 		{#if selfUser && (selfUser.role === 'administrator' || selfUser.role === 'moderator')}
 			<li>
-				<button
-					class="focus:outline-none w-full text-left"
-					type="button"
-					on:click={openRemoveBuildModal}
-				>
-					<i class="fas fa-trash-can w-6 mr-2" />
-					{#if build.removed}
-						Un-Remove
-					{:else}
-						Remove
-					{/if}
-					Build
-					<small>(using moderator privileges)</small>
-				</button>
+				{#if build.removed}
+					<button
+						class="focus:outline-none w-full text-left"
+						type="button"
+						on:click={openUnRemoveBuildModal}
+					>
+						<i class="fas fa-trash-can w-6 mr-2" />
+						Un-Remove Build
+						<small>(using moderator privileges)</small>
+					</button>
+				{:else}
+					<button
+						class="focus:outline-none w-full text-left"
+						type="button"
+						on:click={openRemoveBuildModal}
+					>
+						<i class="fas fa-trash-can w-6 mr-2" />
+						Remove Build
+						<small>(using moderator privileges)</small>
+					</button>
+				{/if}
 			</li>
 		{/if}
 	</ul>
