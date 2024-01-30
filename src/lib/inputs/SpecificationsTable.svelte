0932e09ba17ef696582544839f0e4c9d30970023
Mark Connelly <com0004@gmail.com>
2024-01-30 23:42:25 +1100

    Improve specification table and chips

--- src/lib/inputs/SpecificationsTable.svelte
+++ src/lib/inputs/SpecificationsTable.svelte
@@ -54,14 +54,23 @@
 				.sort();
 		}, 1);
 	}
+
+	const numberPattern = /^-?[0-9]+(\.[0-9]+)?$/;
+
+	function onInput(e: Event, specId: number) {
+		const target = e.target as HTMLInputElement;
+		if (target.value.match(numberPattern)) {
+			specValues[specId] = Number(target.value);
+		}
+	}
 </script>
 
 <table class="table table-compact !table-hover">
 	<thead class="text-left">
-		<th>Name</th>
+		<th class="min-w-[150px]">Name</th>
 		<th>
 			<div class="flex justify-between">
-				<div>Value</div>
+				<div class="flex items-center">Value</div>
 				{#if !readonly}
 					<button
 						class="btn btn-sm hover:variant-soft-surface flex gap-2"
@@ -95,22 +104,34 @@
 				{:then data}
 					<td class="table-cell-fit">
 						<div class="font-semibold mb-1 flex">
-							<span class="mr-2 w-[150px] md:w-[300px] truncate inline-block">
-								{data.name}
-							</span>
 							<a href={`/specifications/${data.id}`} class="anchor" target="_blank">
 								<i class="fa-solid fa-up-right-from-square text-sm h-3" />
 							</a>
+							<span class="ml-2 w-[150px] md:w-[350px] lg:w-[550px] truncate inline-block">
+								{data.name}
+							</span>
 						</div>
-						<div class="truncate w-[170px] md:w-[320px]">
+						<div class="truncate w-[170px] md:w-[370px] lg:w-[570px]">
 							{data.description}
 						</div>
 					</td>
 					<td class="">
-						<input type="number" bind:value={specValues[specId]} class="input" {readonly} />
+						<input
+							type="text"
+							value={specValues[specId]}
+							on:input={(e) => onInput(e, specId)}
+							class="input"
+							{readonly}
+							pattern={numberPattern.source}
+							maxlength={30}
+							placeholder="Enter a Number... e.g. -0.01"
+							required
+						/>
 					</td>
 					<td class="table-cell-fit !pl-0">
-						<div class="whitespace-nowrap mt-2 w-[120px] md:w-[150px] text-center truncate">
+						<div
+							class="whitespace-nowrap mt-2 w-[100px] md:w-[130px] lg:w-[160px] text-center truncate"
+						>
 							{data.unit}
 						</div>
 					</td>
@@ -125,7 +146,9 @@
 		{:else}
 			<tr>
 				<td />
-				<td class="w-full">No specifications provided</td>
+				<td class="w-full">
+					<div class="p-6 text-center opacity-50">No specifications provided</div>
+				</td>
 				<td />
 			</tr>
 		{/each}
