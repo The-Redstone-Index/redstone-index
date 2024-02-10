ea81241d1616f22c418aca7bf30f0244f7ddb856
Mark Connelly <com0004@gmail.com>
2024-02-10 22:59:11 +1100

    Try different api for user details, change status code

--- src/routes/api/face/[username]/+server.ts
+++ src/routes/api/face/[username]/+server.ts
@@ -4,13 +4,11 @@ import type { RequestHandler } from './$types';
 export const GET: RequestHandler = async ({ params, fetch }) => {
 	try {
 		const username = params.username;
-		const response = await fetch(`https://api.mojang.com/users/profiles/minecraft/${username}`, {
-			headers: { 'User-Agent': 'test' }
-		});
+		const response = await fetch(`https://api.minetools.eu/uuid/${username}`);
 		if (!response.ok) {
-			console.error('Mojang API Error:', response.status, response.statusText);
+			console.error('MineTools API Error:', response.status, response.statusText);
 			if (response.status === 404) throw error(response.status, 'Minecraft face does not exist');
-			throw error(response.status, 'Failed to get Mojang resource');
+			throw error(500, 'Failed to get MineTools resource');
 		}
 		const userDetails: { id: string } = await response.json();
 		const userId = userDetails.id;
@@ -18,7 +16,7 @@ export const GET: RequestHandler = async ({ params, fetch }) => {
 		const faceResponse = await fetch(faceUrl);
 		if (!faceResponse.ok) {
 			console.error('Crafatar API Error:', faceResponse.status, faceResponse.statusText);
-			throw error(faceResponse.status, 'Failed to get Crafatar resource');
+			throw error(500, 'Failed to get Crafatar resource');
 		}
 		return faceResponse;
 	} catch (e) {
