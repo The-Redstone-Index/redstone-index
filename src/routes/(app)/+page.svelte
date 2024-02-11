2b6191ebafbe0d9a49785c18f72ef9d508778536
Mark Connelly <com0004@gmail.com>
2024-02-11 14:43:35 +1100

    Add link to about, add logo boop

--- src/routes/(app)/+page.svelte
+++ src/routes/(app)/+page.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+	import Boop from '$lib/common/Boop.svelte';
 	import GlowingRedstoneLogo from '$lib/common/GlowingRedstoneLogo.svelte';
 	import BuildCarousel from '$lib/display/BuildCarousel.svelte';
 
@@ -16,17 +17,23 @@
 </svelte:head>
 
 <div class="container h-full mx-auto justify-center items-center p-4 mb-10">
-	<div class="mt-6 md:mt-12">
-		<GlowingRedstoneLogo size="lg" pulse class="mx-auto hidden md:block" />
-		<GlowingRedstoneLogo size="md" pulse class="mx-auto md:hidden" />
+	<div class="mt-6 md:mt-12 flex justify-center">
+		<Boop>
+			<GlowingRedstoneLogo size="lg" pulse class="mx-auto hidden md:block" />
+			<GlowingRedstoneLogo size="md" pulse class="mx-auto md:hidden" />
+		</Boop>
 	</div>
 
-	<div class="space-y-10 text-center mt-8 mb-10">
+	<div class="space-y-10 text-center mt-8 mb-6">
 		<h2 class="font-bold h2">
 			Welcome to
 			<span class="text-redstone whitespace-nowrap">The Redstone Index</span>
 		</h2>
-		<p class="!mt-6 md:!mt-10">The place where people put redstone things.</p>
+		<p class="!mt-6 md:!mt-10">The place to share and discover redstone things!</p>
+	</div>
+
+	<div class="flex justify-center mb-10">
+		<a href="/about" class="btn variant-filled-primary">About this platform</a>
 	</div>
 
 	<section class="my-4 px-2">
