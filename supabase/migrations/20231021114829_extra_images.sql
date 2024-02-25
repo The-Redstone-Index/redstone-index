4982c91a9ae3f82aba1a55e6900445a4ad3d9158
Mark Connelly <com0004@gmail.com>
2024-02-26 02:32:43 +1100

    Add storage clearing for build extra images

--- supabase/migrations/20231021114829_extra_images.sql
+++ supabase/migrations/20231021114829_extra_images.sql
@@ -13,7 +13,16 @@ create policy "Images are publicly accessible." on storage.objects
 create policy "Anyone can upload an image into their own folder." on storage.objects
     for insert
         with check (bucket_id = 'images'
-        and (storage.foldername(name))[1] = auth.uid()::text);
+        -- Check if the user is inserting into their own folder
+        and (storage.foldername(name))[1] = auth.uid()::text
+        -- Check if the user owns the associated schematic
+        and exists (
+            select
+                1
+            from
+                schematics s
+            where
+                s.id =((storage.foldername(name))[2])::int and s.user_id = auth.uid()));
 
 create policy "Image owner can delete their own images." on storage.objects
     for delete
