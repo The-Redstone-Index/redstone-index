81932af608d86c7bf7b90d18bd1e7249cce0f82c
Mark Connelly <com0004@gmail.com>
2024-02-26 13:44:18 +1100

    move avatars to own migration file

--- supabase/migrations/20230822060601_users.sql
+++ supabase/migrations/20230822060601_users.sql
@@ -165,40 +165,6 @@ create trigger on_auth_user_created
 
 
 /*
- * Avatars bucket
- * Max 3MB. Image files only.
- */
-insert into storage.buckets(id, name, public, file_size_limit, allowed_mime_types)
-    values ('avatars', 'avatars', true, 3000000, array['image/jpeg', 'image/png', 'image/gif', 'image/webp']);
-
--- RLS
-create policy "Avatar images are publicly accessible." on storage.objects
-    for select
-        using (bucket_id = 'avatars');
-
-create policy "Anyone can upload an avatar into their own folder." on storage.objects
-    for insert
-        with check (
-        -- Avatars bucket
-        bucket_id = 'avatars'
-        -- Check the user is uploading into their own folder
-        and (storage.foldername(name))[1] = auth.uid()::text
-        -- Max 5 images uploaded to this folder
-        and (
-            select
-                COUNT(*)
-            from
-                storage.objects so
-            where
-                so.bucket_id = 'avatars' and storage.foldername(so.name) = storage.foldername(name)) < 5);
-
-create policy "Image owner can delete their own avatar." on storage.objects
-    for delete
-        using (bucket_id = 'avatars'
-            and auth.uid() = owner);
-
-
-/*
  * Is user a member?
  */
 create or replace function auth.is_member()
